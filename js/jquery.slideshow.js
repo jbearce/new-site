48d7c2bd2005205023b44d44bde35a89b005f242
Jacob Bearce <jacob@bearce.me>
2015-01-21 21:03:33 -0600

    Clean Up
    De-weblinxed, genercized, standardized

--- js/jquery.slideshow.js
+++ js/jquery.slideshow.js
@@ -6,11 +6,11 @@ var slideTime = 5000;
 var nextSlide = 1;
 var numberOfSlides = 0;
 $(document).ready(function() {
-	$("#slideshow #slideshowWindow figure").each(function() {
+	$("#slideshow figure").each(function() {
 		numberOfSlides++;
 		$(this).attr("data-slide",numberOfSlides);
 	});
-	$("#slideshow #slideshowWindow figure").first().addClass("active");
+	$("#slideshow figure").first().addClass("active");
 });
 function autoRotate() {
 	if (nextSlide != numberOfSlides) {
@@ -18,14 +18,14 @@ function autoRotate() {
 	} else {
 		nextSlide = 1;
 	}
-	$("#slideshow #slideshowWindow figure.active").removeClass("active");
-	$("#slideshow #slideshowWindow figure[data-slide=" + nextSlide + "]").addClass("active");
+	$("#slideshow figure.active").removeClass("active");
+	$("#slideshow figure[data-slide=" + nextSlide + "]").addClass("active");
 }
 var slideLoop;
 slideLoop = setInterval(function() {
 	autoRotate();
 }, slideTime);
-$("#slideshowWindow").hover(function() {
+$("#slideshow").hover(function() {
 	clearInterval(slideLoop);
 }, function() {
 	slideLoop = setInterval(function() {
@@ -35,18 +35,18 @@ $("#slideshowWindow").hover(function() {
 $("#slideshow > button").click(function() {
 	var direction = $(this).attr("data-direction");
 	if (direction == "next") {
-		nextSlide = parseInt($("#slideshow #slideshowWindow figure.active").attr("data-slide")) + 1;
+		nextSlide = parseInt($("#slideshow figure.active").attr("data-slide")) + 1;
 		if (nextSlide == numberOfSlides + 1) {
 			nextSlide = 1;
 		}
 	} else {
-		nextSlide = parseInt($("#slideshow #slideshowWindow figure.active").attr("data-slide")) - 1;
+		nextSlide = parseInt($("#slideshow figure.active").attr("data-slide")) - 1;
 		if (nextSlide == 0) {
 			nextSlide = numberOfSlides;
 		}
 	}
-	$("#slideshow #slideshowWindow figure.active").removeClass("active");
-	$("#slideshow #slideshowWindow figure[data-slide=" + nextSlide + "]").addClass("active");
+	$("#slideshow figure.active").removeClass("active");
+	$("#slideshow figure[data-slide=" + nextSlide + "]").addClass("active");
 	clearInterval(slideLoop);
 	slideLoop = setInterval(function() {
 		autoRotate()
