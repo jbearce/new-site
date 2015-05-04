6cbb818290fd8ad42a16235b0306c64fd6a08dac
Jacob Bearce <jacob@bearce.me>
2015-05-03 19:54:15 -0500

    Gulp stuff
    - Moved static site to src
    - Modified github to ignore dist
    - Set up package.json to enable quick installation of gulp
    - Enabled HTML includes
    - SCSS compilation with auto prefixer
    - JS concater (uglifier doesn't work currently)
    - Image optimizer
    - Watcher with live reloading
    - Probably other stuff I'm forgetting

--- src/error.php
+++ src/error.php
@@ -1,9 +1,36 @@
-<p>Sorry, it looks like something went wrong.</p>
-<?
-if ($_GET["error"]) {
-    echo "<p>" . $_GET["error"] . "</p>";
-}
-if ($_GET["extra"]) {
-    echo "<p>" . $_GET["extra"] . "</p>";
-}
-?>
+<!doctype html>
+<html lang="en-US">
+	<head>
+        @@include("./includes/head.htm")
+        <!-- SEO -->
+        <meta content="156 characters with spaces" name="description" />
+        <title>Error</title>
+        <!-- end SEO -->
+	</head>
+	<body>
+        <section id="pageWrapper">
+            @@include("./includes/header.htm")
+            <section id="mainWrapper">
+                <main>
+                    <section id="post">
+                        <p>Sorry, it looks like something went wrong.</p>
+                        <?
+                        if ($_GET["error"]) {
+                            echo "<p>" . $_GET["error"] . "</p>";
+                        }
+                        if ($_GET["extra"]) {
+                            echo "<p>" . $_GET["extra"] . "</p>";
+                        }
+                        ?>
+                    </section><!--/#post-->
+                    <section id="sidebar">
+                        @@include("./includes/sidebar.htm")
+                    </section><!--/#sidebar-->
+                </main>
+            </section><!--/#mainWrapper-->
+            @@include("./includes/footer.htm")
+        </section><!--/#pageWrapper-->
+        @@include("./includes/nav-mobile.htm")
+        @@include("./includes/foot.htm")
+	</body>
+</html>
