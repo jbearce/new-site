725dac480855c2a2f9839e29529b0732cc58ae5f
Jacob Bearce <jacob@bearce.me>
2015-06-03 16:59:14 -0500

    .htaccess, clean-up, bug fixing

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,3 @@
-//@prepros-append vendors/jquery-1.11.2.js
 //@prepros-append vendors/modernizr.custom.79241.js
 //@prepros-append vendors/scrollfix.js
 //@prepros-append jquery.modernizr.cssForms.js
