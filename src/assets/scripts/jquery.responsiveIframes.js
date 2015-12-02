46ac87bac2689c6ef05c5af6e025c56365824ebe
Jacob Bearce <jacob@bearce.me>
2015-12-02 16:41:46 -0600

    Adjusted automatic responsive iframes to only work for youtube, vimeo, or daily motion
    Will add more as needed in the future.

--- src/assets/scripts/jquery.responsiveIframes.js
+++ src/assets/scripts/jquery.responsiveIframes.js
@@ -5,10 +5,13 @@
 // responsive iframes
 $(document).ready(function () {
     "use strict";
-	$("iframe").each(function () {
-		var height = $(this).attr("height"),
-		    width = $(this).attr("width"),
-		    aspectRatio = (height / width) * 100 + "%";
-		$(this).wrap("<span class='iframe' style='padding-bottom:" + aspectRatio + "'>");
+	$("iframe").not(".iframe iframe").each(function () {
+        var src = $(this).attr("src");
+        if ((src.indexOf("youtube") > -1) || (src.indexOf("vimeo") > -1) || (src.indexOf("dailymotion") > -1)) {
+            var height = $(this).attr("height"),
+                width = $(this).attr("width"),
+                aspectRatio = (height / width) * 100 + "%";
+            $(this).wrap("<span class='iframe' style='padding-bottom:" + aspectRatio + "'>");
+        }
 	});
 });
