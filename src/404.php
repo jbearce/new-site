fe72a95aa09470327fd39d1866ec561f06a0aaf8
Jacob Bearce <jacob@bearce.me>
2016-03-02 16:31:30 -0600

    Moved the breadcrumbs out of article-card in 404.php
    More consistent with other pages

--- src/404.php
+++ src/404.php
@@ -2,13 +2,13 @@
             <div class="content-wrapper">
                 <main class="content-block">
                     <div class="post">
+                        <?php
+                        // display breadcrumbs
+                        if (function_exists("yoast_breadcrumb")) {
+                            yoast_breadcrumb("<nav class='breadcrumb-list'><p class='text'>", "</p></nav>");
+                        }
+                        ?>
                         <article class="article-card">
-                            <?php
-                            // display breadcrumbs
-                            if (function_exists("yoast_breadcrumb")) {
-                                yoast_breadcrumb("<nav class='breadcrumb-list'><p class='text'>", "</p></nav>");
-                            }
-                            ?>
                             <header class="header">
                                 <h1 class="title">404: Page Not Found</h1>
                             </header><!--/.header-->
