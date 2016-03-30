c9646f9aa97425af2ee29c846d9e99f2f3850f93
Jacob Bearce <jacob@bearce.me>
2016-03-30 10:23:59 -0500

    Changes content class to text in searchform.php

--- src/searchform.php
+++ src/searchform.php
@@ -1,6 +1,6 @@
 <?php $search_query = is_search() ? get_search_query() : ""; ?>
 <form class="search-form" action="<?php echo home_url(); ?>" method="get">
-    <label class="search-label content" for="s"><?php _e("Search for:", "new-site"); ?></label>
+    <label class="search-label text" for="s"><?php _e("Search for:", "new-site"); ?></label>
     <input class="search-input input" name="s" title="<?php _e("Search for:", "new-site"); ?>" type="search" value="<?php echo $search_query; ?>" />
     <button class="search-submit button" type="submit"><?php _e("Search", "new-site"); ?></button>
 </form><!--/.search-form-->
