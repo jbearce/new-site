f0ef45b22c80cb53e59614108c164006c125c798
Jacob Bearce <jacob@bearce.me>
2016-04-02 14:05:06 -0500

    Default search form styles

--- src/searchform.php
+++ src/searchform.php
@@ -1,6 +1,6 @@
 <?php $search_query = is_search() ? get_search_query() : ""; ?>
 <form class="search-form" action="<?php echo home_url(); ?>" method="get">
-    <label class="search-label text" for="s"><?php _e("Search for:", "new-site"); ?></label>
+    <label class="search-label text _visuallyhidden" for="s"><?php _e("Search for:", "new-site"); ?></label>
     <input class="search-input input" name="s" title="<?php _e("Search for:", "new-site"); ?>" type="search" value="<?php echo $search_query; ?>" />
-    <button class="search-submit button" type="submit"><?php _e("Search", "new-site"); ?></button>
+    <button class="search-submit button" type="submit"><i class="fa fa-search"></i><span class="_visuallyhidden"><?php _e("Search", "new-site"); ?></span></button>
 </form><!--/.search-form-->
