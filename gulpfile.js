2bb818355640d363806641878190c000601ce117
Jacob Bearcce <jacob@bearce.me>
2016-07-11 14:42:37 -0500

    Fix an issue where scripts task would always run

--- gulpfile.js
+++ gulpfile.js
@@ -144,7 +144,7 @@ gulp.task("scripts", function () {
     // lint scripts
     var linted = gulp.src([src + "/assets/scripts/*.js", "!" + src + "/assets/scripts/vendor.*.js"])
         // check if source is newer than destination
-        .pipe(gulpif(!argv.force, newer(jsDirectory)))
+        .pipe(gulpif(!argv.force, newer(jsDirectory + "/all.js")))
         // lint all non-vendor scripts
         .pipe(jshint())
         // print lint errors
