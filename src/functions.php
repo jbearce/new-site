26ac1bb74c980268581e903436abfa7bac6b52c6
Jacob Bearcce <jacob@bearce.me>
2016-07-13 14:30:06 -0500

    Add get_better_excerpt function; Close #46

--- src/functions.php
+++ src/functions.php
@@ -141,3 +141,18 @@ function new_site_enqueue_scripts() {
     wp_enqueue_script("jquery");
 }
 add_action("wp_enqueue_scripts", "new_site_enqueue_scripts");
+
+/* ------------------------------------------------------------------------ *\
+ * Custom Functions
+\* ------------------------------------------------------------------------ */
+
+// get a nicer excerpt based on post ID
+function get_better_excerpt($id = 0, $length = 55, $more = " [...]") {
+    global $post;
+
+    $post_id = $id ? $id : $post->ID;
+    $post_object = get_post($post_id);
+    $excerpt = $post_object->post_excerpt ? $post_object->post_excerpt : wp_trim_words(strip_shortcodes($post_object->post_content), $length, $more);
+
+    return $excerpt;
+}
