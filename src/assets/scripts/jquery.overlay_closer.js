d4b90e12ca34fe500f11d40ac210ec9440216762
Jacob Bearcce <jacob@bearce.me>
2016-08-18 08:19:43 -0500

    Enable esc to close overlay

--- src/assets/scripts/jquery.overlay_closer.js
+++ src/assets/scripts/jquery.overlay_closer.js
@@ -12,3 +12,16 @@ jQuery(".overlay-closer").click(function(e) {
     jQuery(this).removeClass("is-active");
     jQuery(this).attr("aria-hidden", "true");
 });
+
+// close the overlay and any associated elements when escape key is pressed
+jQuery(document).keyup(function(e) {
+    if (jQuery("[data-overlay][aria-hidden=false]").length && e.keyCode === 27) {
+        var closing_overlay = jQuery("[data-overlay][aria-hidden=false]").data("overlay");
+
+        jQuery("[data-overlay=" + closing_overlay + "]").not("[aria-hidden]").focus();
+        jQuery("[data-overlay]").removeClass("is-active");
+        jQuery("[data-overlay][aria-hidden]").attr("aria-hidden", "true");
+        jQuery(".overlay-closer").removeClass("is-active");
+        jQuery(".overlay-closer").attr("aria-hidden", "true");
+    }
+});
