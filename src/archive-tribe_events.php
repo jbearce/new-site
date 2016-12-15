b04783ae912c8b9b7b52981cf62ad4467b44d677
Jacob Bearce <jacob@bearce.me>
2016-12-15 14:56:18 -0600

    Tribe Events bar

---
+++ src/archive-tribe_events.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<?php get_template_part("partials/hero", "block"); ?>
+<div class="content-block">
+    <div class="content_inner">
+        <div class="content_post">
+            <?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
+                    the_content();
+                }
+            }
+            ?>
+        </div><!--/.content_post-->
+    </div><!--/.content_inner-->
+</div><!--/.content-block-->
+<?php get_footer(); ?>
--- src/page.php
+++ src/page.php
@@ -1,3 +1,13 @@
+<?php
+// force redirect 'cause tribe is stupid
+if (is_post_type_archive("tribe_events")) {
+    include(TEMPLATEPATH . "/archive-tribe_events.php");
+    return;
+} elseif (get_post_type() == "tribe_events") {
+    include(TEMPLATEPATH . "/single-tribe_events.php");
+    return;
+}
+?>
 <?php get_header(); ?>
 <?php get_template_part("partials/hero", "block"); ?>
 <div class="content-block">
