9e239df4fbd19715b03aa8251e86f270b32c2777
Jacob Bearce <jacob@bearce.me>
2017-01-25 16:14:48 -0600

    Add new_site_ prefix to some filters

--- src/functions/page-speed.php
+++ src/functions/page-speed.php
@@ -12,19 +12,19 @@ add_filter("script_loader_src", "new_site_remove_script_version", 15, 1);
 add_filter("style_loader_src", "new_site_remove_script_version", 15, 1);
 
 // disable oEmbed
-function speed_stop_loading_wp_embed() {
+function new_site_stop_loading_wp_embed() {
     if (!is_admin()) {
         wp_deregister_script("wp-embed");
     }
 }
-add_action("init", "speed_stop_loading_wp_embed");
+add_action("init", "new_site_stop_loading_wp_embed");
 
 // disable Emoji
 remove_action("wp_head", "print_emoji_detection_script", 7);
 remove_action("wp_print_styles", "print_emoji_styles");
 
 // load scripts asynchronously
-function make_scripts_async($tag, $handle, $src) {
+function new_site_make_scripts_async($tag, $handle, $src) {
     if (!is_admin()) {
         return str_replace("<script", "<script defer='defer'", $tag);
         exit;
@@ -32,10 +32,10 @@ function make_scripts_async($tag, $handle, $src) {
 
     return $tag;
 }
-add_filter("script_loader_tag", "make_scripts_async", 10, 3);
+add_filter("script_loader_tag", "new_site_make_scripts_async", 10, 3);
 
 // load styles asynchronously
-function make_styles_async($tag, $handle, $src) {
+function new_site_make_styles_async($tag, $handle, $src) {
     if (!is_admin()) {
         return str_replace("rel='stylesheet'", "rel='preload' as='style' onload=\"this.rel='stylesheet'\"", $tag) . "<noscript>{$tag}</noscript>";
         exit;
@@ -43,4 +43,4 @@ function make_styles_async($tag, $handle, $src) {
 
     return $tag;
 }
-add_filter("style_loader_tag", "make_styles_async", 10, 3);
+add_filter("style_loader_tag", "new_site_make_styles_async", 10, 3);
