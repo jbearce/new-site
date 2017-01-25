65ca030369fb7fa6bfa8f7f7f48f208cc2f219f9
Jacob Bearce <jacob@bearce.me>
2017-01-25 17:07:57 -0600

    Convert home.php to mixed PHP & HTML

--- src/home.php
+++ src/home.php
@@ -6,11 +6,16 @@
             <?php
             $posts_page = get_post(get_option("page_for_posts"));
             $title = get_the_title($posts_page->ID) ? get_the_title($posts_page->ID) : __("Latest Posts", "new_site");
-
-            if ($title) {
-                echo "<article class='content_article article -introduction'><header class='article_header'><h1 class='article_title title'>{$title}</h1></header></article>";
-            }
             ?>
+
+            <?php if ($title): ?>
+                <article class="content_article article -introduction">
+                    <header class="article_header">
+                        <h1 class="article_title title"><?php echo $title; ?></h1>
+                    </header><!--/.article_header-->
+                </article><!--/.content_article.article.-introduction-->
+            <?php endif; ?>
+            
             <?php get_template_part("partials/content", "excerpt"); ?>
             <?php get_template_part("partials/pagination", "list"); ?>
         </div><!--/.content_post-->
