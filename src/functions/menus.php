65c73136dd4791df29ed4800dbfa9a0cb80e0f66
Jacob Bearce <jacob@bearce.me>
2017-02-21 09:24:12 -0600

    Add missing show_parent parameter to new_site_nav_menu_sub_menu

--- src/functions/menus.php
+++ src/functions/menus.php
@@ -263,6 +263,7 @@ if (is_admin()) {
 
 // add sub_menu options to wp_nav_menu
 // @param  {Boolean}  sub_menu - Make a menu behave as a sub menu
+// @param  {Boolean}  show_parent - Set to false to hide the parent menu item, thus only showing siblings
 // @param  {Boolean}  direct_parent - Only show the parent of the currently viewed page, not any further ancestors
 // @param  {Number}   parent_id - Post ID to use as the parent
 function new_site_nav_menu_sub_menu($menu_items, $args) {
