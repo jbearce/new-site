dcd7c56708216fd74e2d2cf7725e9784db2f8bf0
Jacob Bearce <jacob@bearce.me>
2017-02-28 09:26:39 -0600

    Improve JSON sitemap functionality

--- src/functions/filters.php
+++ src/functions/filters.php
@@ -5,19 +5,78 @@
 
 // generate JSON sitemap for uncss & critical
 function new_site_json_sitemap() {
-    if (isset($_GET["sitemap"])) {
-        $sitemap_query = new WP_Query(array(
+    if (isset($_GET["sitemap"]) === true) {
+        $urls = array();
+
+        /* get one 404 URL */
+
+        $urls[] = home_url() . "/" . uniqid();
+
+        /* get one search URL */
+
+        $urls[] = home_url() . "/?s=" . get_bloginfo("name");
+
+        /* get one URL for each post type single */
+        /* @TODO get one for each post template, not simply each post type */
+
+        // store linked post types in an array
+        $linked_post_types = array();
+        // get all posts of any post type
+        $post_type_query = new WP_Query(array(
             "post_status" => "publish",
             "post_type"   => "any",
             "showposts"   => "-1",
         ));
-        $urls = array();
 
-        while ($sitemap_query->have_posts()) {
-            $sitemap_query->the_post();
-            $urls[] = get_permalink();
+        // loop through all posts of any post type
+        while ($post_type_query->have_posts()) {
+            // iterate the post index
+            $post_type_query->the_post();
+
+            // get the current post type
+            $current_post_type = get_post_type();
+
+            // get the current post type archive link
+            $current_post_type_archive_link = get_post_type_archive_link($current_post_type);
+
+            // check if the current post type has alread been linked
+            if ($current_post_type && !in_array($current_post_type, $linked_post_types) && !in_array(get_permalink(), $urls)) {
+                // add the post type to the Linked Post Types array
+                $linked_post_types[] = $current_post_type;
+                // add the URL to the URLs array
+                $urls[] = get_permalink();
+
+                // check if the current post type archive link has already been linked
+                if ($current_post_type_archive_link && !in_array($current_post_type_archive_link, $urls)) {
+                    // add the URL to the URLs array
+                    $urls[] = preg_match("/\/$/", $current_post_type_archive_link) ? $current_post_type_archive_link : $current_post_type_archive_link . "/";
+                }
+            }
         }
 
+        /* get one URL for each taxonomy */
+        /* @TODO get one for each taxonomy template, not simply each taxonomy */
+
+        // store linked taxonomies in an array
+        $linked_taxonomies = array();
+        // get all taxonomies
+        $taxonomy_terms = get_terms(get_taxonomies());
+
+        // loop through all taxonomies
+        foreach ($taxonomy_terms as $term) {
+            // get the current taxonomy
+            $current_taxonomy = get_taxonomy($term->taxonomy);
+            // get the current term link
+            $term_link = get_term_link($term);
+
+            // check if the current taxonomy is public, if it's already been linked, and if a term link exists
+            if ($current_taxonomy && $current_taxonomy->public && !in_array($current_taxonomy->name, $linked_taxonomies) && $term_link) {
+                $linked_taxonomies[] = $current_taxonomy->name;
+                $urls[] = $term_link;
+            }
+        }
+
+        // stop all other code and output the URLs
         die(json_encode($urls));
     }
 }
