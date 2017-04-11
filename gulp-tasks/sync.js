e321a79c7ec76d942e8a3c11b141c6a6a8d52949
Jacob Bearce <jacob@bearce.me>
2017-04-11 13:15:45 -0500

    Fix how sync task gets returned

--- gulp-tasks/sync.js
+++ gulp-tasks/sync.js
@@ -4,10 +4,12 @@
 
 module.exports = function (plugins, bs) {
     // sync task, set up a browser_sync server, depends on config
-    return plugins.browser_sync({
-        proxy:  bs.proxy,
-        port:   bs.port,
-        open:   bs.open   === "true" ? true : (bs.open   === "false" ? false : bs.open),
-        notify: bs.notify === "true" ? true : (bs.notify === "false" ? false : bs.notify),
-    });
+    return function () {
+        plugins.browser_sync({
+            proxy:  bs.proxy,
+            port:   bs.port,
+            open:   bs.open   === "true" ? true : (bs.open   === "false" ? false : bs.open),
+            notify: bs.notify === "true" ? true : (bs.notify === "false" ? false : bs.notify),
+        });
+    };
 };
