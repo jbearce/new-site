bdd2555017fd1f0701a9f3582119d860414bcd95
Jacob Bearce <jacob@bearce.me>
2017-05-01 09:00:26 -0500

    Make PHP function includes a bit more consistent

--- src/functions.php
+++ src/functions.php
@@ -1,11 +1,11 @@
 <?php
-require_once(__DIR__ . "/functions/post-types.php");
-require_once(__DIR__ . "/functions/page-speed.php");
-require_once(__DIR__ . "/functions/theme-features.php");
-require_once(__DIR__ . "/functions/menus.php");
-require_once(__DIR__ . "/functions/styles-scripts.php");
-require_once(__DIR__ . "/functions/image-sizes.php");
-require_once(__DIR__ . "/functions/filters.php");
-require_once(__DIR__ . "/functions/shortcodes.php");
-require_once(__DIR__ . "/functions/custom-functions.php");
-require_once(__DIR__ . "/functions/advanced-custom-fields.php");
+require_once(TEMPLATEPATH . "/functions/post-types.php");
+require_once(TEMPLATEPATH . "/functions/page-speed.php");
+require_once(TEMPLATEPATH . "/functions/theme-features.php");
+require_once(TEMPLATEPATH . "/functions/menus.php");
+require_once(TEMPLATEPATH . "/functions/styles-scripts.php");
+require_once(TEMPLATEPATH . "/functions/image-sizes.php");
+require_once(TEMPLATEPATH . "/functions/filters.php");
+require_once(TEMPLATEPATH . "/functions/shortcodes.php");
+require_once(TEMPLATEPATH . "/functions/custom-functions.php");
+require_once(TEMPLATEPATH . "/functions/advanced-custom-fields.php");
--- src/functions/post-types.php
+++ src/functions/post-types.php
@@ -4,7 +4,7 @@
 \* ------------------------------------------------------------------------ */
 
 // include page-for-post-type plugin
-require_once(TEMPLATEPATH . "/vendor/page-for-post-type.php");
+require_once(TEMPLATEPATH . "/functions/vendor/page-for-post-type.php");
 add_action("after_setup_theme", array("Page_For_Post_Type", "get_instance"));
 
 // register project post type
