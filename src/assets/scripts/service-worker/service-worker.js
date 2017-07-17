47471eac4c42f831951d1b066f4ca9c0f63c48cf
Jacob Bearce <jacob@bearce.me>
2017-07-17 14:06:08 -0500

    Don't register the service worker on previews

--- src/assets/scripts/service-worker/service-worker.js
+++ src/assets/scripts/service-worker/service-worker.js
@@ -2,6 +2,13 @@
 
 // Scripts written by YOURNAME @ YOURCOMPANY
 
+// no service worker for previews
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.match(/preview=true/)) {
+        return;
+    }
+});
+
 toolbox.precache(["/", "../styles/modern.css"]);
 toolbox.router.get("../media/*", toolbox.cacheFirst);
 toolbox.router.get("/wp-content/uploads/*", toolbox.cacheFirst);
