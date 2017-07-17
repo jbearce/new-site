50586df6a1db5052a7a112789d65922403d8f1ce
Jacob Bearce <jacob@bearce.me>
2017-07-17 13:43:58 -0500

    Update root URL in service worker to have utm_source

--- src/assets/scripts/service-worker/service-worker.js
+++ src/assets/scripts/service-worker/service-worker.js
@@ -2,7 +2,7 @@
 
 // Scripts written by YOURNAME @ YOURCOMPANY
 
-toolbox.precache(["/", "../styles/modern.css"]);
+toolbox.precache(["?utm_source=pwa", "../styles/modern.css"]);
 toolbox.router.get("../media/*", toolbox.cacheFirst);
 toolbox.router.get("/wp-content/uploads/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, {NetworkTimeoutSeconds: 5});
