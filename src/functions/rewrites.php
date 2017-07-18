08f048c9659f9eeac8aa2922533b6847c1403d5e
Jacob Bearce <jacob@bearce.me>
2017-07-18 09:15:20 -0500

    Add variable replacement to scripts task

--- src/functions/rewrites.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* ------------------------------------------------------------------------ *\
- * Functions: Rewrites
-\* ------------------------------------------------------------------------ */
-
-function new_site_rewrite_rules() {
-    add_rewrite_rule("service-worker\.js$", substr(wp_make_link_relative(get_bloginfo("template_directory")), 1) . "/assets/scripts/service-worker.js", "top");
-}
-add_action("init", "new_site_rewrite_rules");
