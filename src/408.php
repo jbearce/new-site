1e54e05b5ad7b9882af44b897eac85f4f7dad0f9
Jacob Bearce <jacob@bearce.me>
2017-07-24 16:44:29 -0500

    Attempt to serve custom offline error page

---
+++ src/408.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<?php get_template_part("partials/block", "hero"); ?>
+<div class="content-block">
+    <div class="content_inner">
+        <div class="content_post">
+            <article class="content_article article">
+                <header class="article_header">
+                    <h1 class="article_title title"><?php _e("408: Request Timeout"); ?></h1>
+                </header><!--/.article_header-->
+                <div class="article_content">
+                    <p class="article_text text"><?php _e("You may be seeing this error because you are offline. You can continue to browse to pages you have previously visited; to visit new pages, please verify your connection.", "new_site"); ?></p>
+                </div><!--/.article_content-->
+            </article><!--/.content_article.article-->
+        </div><!--/.content_post-->
+    </div><!--/.content_inner-->
+</div><!--/.content-block-->
+<?php get_footer(); ?>
--- src/assets/scripts/service-worker/service-worker.js
+++ src/assets/scripts/service-worker/service-worker.js
@@ -15,7 +15,18 @@
     global.addEventListener("activate", event => event.waitUntil(global.clients.claim()));
 
     // set up the cache
-    global.toolbox.precache(["/"]);
+    global.toolbox.precache(["/", "/offline"]);
     global.toolbox.router.get("/wp-content/uploads/*", toolbox.cacheFirst);
     global.toolbox.router.get("/*", toolbox.networkFirst, {NetworkTimeoutSeconds: 5});
+
+    // redirect offline queries to offline page
+    self.toolbox.router.get("/(.*)", function (req, vals, opts) {
+        return toolbox.networkFirst(req, vals, opts).catch((error) => {
+            if (req.method === "GET" && req.headers.get("accept").includes("text/html")) {
+                return toolbox.cacheOnly(new Request("/offline"), vals, opts);
+            }
+
+            throw error;
+        });
+    });
 })(self);
