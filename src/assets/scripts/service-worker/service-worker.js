62828cf7d337af0278b51a2ab79525da691e6984
Jacob Bearce <jacob@bearce.me>
2017-07-24 10:20:32 -0500

    Attempt to serve pages offline

--- src/assets/scripts/service-worker/service-worker.js
+++ src/assets/scripts/service-worker/service-worker.js
@@ -14,3 +14,13 @@ toolbox.precache(["/", "../media/logo.svg", "../media/spritesheet.svg", "../scri
 toolbox.router.get("../media/*", toolbox.cacheFirst);
 toolbox.router.get("/wp-content/uploads/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, {NetworkTimeoutSeconds: 5});
+
+// show offline error
+self.toolbox.router.get("/(.*)", function (req, vals, opts) {
+    return toolbox.networkFirst(req, vals, opts).catch(function (error) {
+        if (req.method === "GET" && req.headers.get("accept").includes("text/html")) {
+            return toolbox.cacheOnly(new Request("/offline"), vals, opts);
+        }
+        throw error;
+    });
+});
