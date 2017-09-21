eb097087a7dc0485fc2c806e33c3b6ef7cdb509d
Jacob Bearce <jacob@bearce.me>
2017-09-21 09:51:56 -0500

    Fix a typo

--- gulp-tasks/init.js
+++ gulp-tasks/init.js
@@ -20,7 +20,7 @@ module.exports = (gulp, plugins) => {
                     // go to next file
                     next(null, file);
                 }))
-                // remove resource code if --remove tribe is passed
+                // remove resource code if --remove resources is passed
                 .pipe(plugins.remove_code({resources_html: plugins.argv.remove && plugins.argv.remove.includes("resources") ? true : false, commentStart: "<!--", commentEnd: "-->"}))
                 .pipe(plugins.remove_code({resources_css_js_php: plugins.argv.remove && plugins.argv.remove.includes("resources") ? true : false, commentStart: "/*", commentEnd: "*/"}))
                 // remove tribe code if --remove tribe is passed
