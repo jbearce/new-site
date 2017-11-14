eeafe58ebf2dc8cea253e0ae4e7a773c70cf0f59
Jacob Bearce <jacob@bearce.me>
2017-11-14 13:29:27 -0600

    Improve content parsing

--- src/functions/filters.php
+++ src/functions/filters.php
@@ -47,11 +47,39 @@ function new_site_tinymce_settings($settings) {
 }
 add_filter("tiny_mce_before_init", "new_site_tinymce_settings");
 
+// fix shortcode formatting
+function new_site_fix_shortcodes($content) {
+    if (!is_admin()) {
+        $array = array (
+            "<p>["                => "[",
+            "]</p>"               => "]",
+            "]<br />"             => "]",
+
+            "<p>&#91;"            => "[",
+            "&#93;</p>"           => "]",
+            "&#93;<br />"         => "]",
+
+            "&#60;p&#62;["        => "[",
+            "]&#60;/p&#62;"       => "]",
+            "[&#60;br /&#62;"     => "]",
+
+            "&#60;p&#62;&#91;"    => "[",
+            "&#93;&#60;/p&#62;"   => "]",
+            "&#93;&#60;br /&#62;" => "]",
+        );
+        $content = strtr($content, $array);
+    }
+
+    return $content;
+}
+add_filter("the_content", "new_site_fix_shortcodes", 5);
+add_filter("acf_the_content", "new_site_fix_shortcodes", 5);
+
 // add classes to elements
 function new_site_add_user_content_classes($content) {
     if ($content) {
         $DOM = new DOMDocument();
-        $DOM->loadHTML($content);
+        $DOM->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
         $anchors = $DOM->getElementsByTagName("a");
 
@@ -182,31 +210,14 @@ function new_site_add_user_content_classes($content) {
         return $DOM->saveHTML();
     }
 }
-add_filter("the_content", "new_site_add_user_content_classes");
-add_filter("acf_the_content", "new_site_add_user_content_classes");
-
-// fix shortcode formatting
-function new_site_fix_shortcodes($content) {
-	$array = array (
-		"<p>["         => "[",
-		"]</p>"        => "]",
-		"]<br />"      => "]",
-        "<p>&#91;"     => "[",
-        "&#93;</p>"    => "]",
-        "&#93;<br />"  => "]",
-	);
-	$content = strtr($content, $array);
-
-    return $content;
-}
-add_filter("the_content", "new_site_fix_shortcodes");
-add_filter("acf_the_content", "new_site_fix_shortcodes", 12);
+add_filter("the_content", "new_site_add_user_content_classes", 999);
+add_filter("acf_the_content", "new_site_add_user_content_classes", 999);
 
 // wrap tables in a div
 function new_site_wrap_tables($content) {
     if ($content) {
         $DOM = new DOMDocument();
-        $DOM->loadHTML($content);
+        $DOM->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
         $tables = $DOM->getElementsByTagName("table");
 
@@ -222,14 +233,14 @@ function new_site_wrap_tables($content) {
         return $DOM->saveHTML();
     }
 }
-add_filter("the_content", "new_site_wrap_tables");
-add_filter("acf_the_content", "new_site_wrap_tables");
+add_filter("the_content", "new_site_wrap_tables", 999);
+add_filter("acf_the_content", "new_site_wrap_tables", 999);
 
 // wrap frames in a div
 function new_site_wrap_frames($content) {
     if ($content) {
         $DOM = new DOMDocument();
-        $DOM->loadHTML($content);
+        $DOM->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
         $iframes = $DOM->getElementsByTagName("iframe");
 
@@ -272,32 +283,14 @@ function new_site_wrap_frames($content) {
         return $DOM->saveHTML();
     }
 }
-add_filter("the_content", "new_site_wrap_frames");
-add_filter("acf_the_content", "new_site_wrap_frames");
-
-// remove dimensions from thumbnails
-function new_site_remove_thumbnail_dimensions($html, $post_id, $post_image_id) {
-    if ($html) {
-        $DOM = new DOMDocument();
-        $DOM->loadHTML($html);
-
-        $images = $DOM->getElementsByTagName("img");
-
-        foreach ($images as $image) {
-            $image->removeAttribute("height");
-            $image->removeAttribute("width");
-        }
-
-        return $DOM->saveHTML();
-    }
-}
-add_filter("post_thumbnail_html", "new_site_remove_thumbnail_dimensions", 10, 3);
+add_filter("the_content", "new_site_wrap_frames", 999);
+add_filter("acf_the_content", "new_site_wrap_frames", 999);
 
 // add rel="noopener" to external links
 function new_site_rel_noopener($content) {
     if ($content) {
         $DOM = new DOMDocument();
-        $DOM->loadHTML($content);
+        $DOM->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
         $anchors = $DOM->getElementsByTagName("a");
 
@@ -312,8 +305,71 @@ function new_site_rel_noopener($content) {
         return $DOM->saveHTML();
     }
 }
-add_filter("the_content", "new_site_rel_noopener");
-add_filter("acf_the_content", "new_site_rel_noopener", 12);
+add_filter("the_content", "new_site_rel_noopener", 999);
+add_filter("acf_the_content", "new_site_rel_noopener", 999);
+
+// enable lazy loading on images
+function new_site_lazy_load_images($content) {
+    if ($content && !((function_exists("tribe_is_month") && tribe_is_month()) && !is_tax())) {
+        $DOM   = new DOMDocument();
+
+        $DOM->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+        $XPath = new DOMXPath($DOM);
+
+        $images = $XPath->query("//img");
+
+        foreach ($images as $image) {
+            $existing_src    = $image->getAttribute("src");
+            $existing_srcset = $image->getAttribute("srcset");
+
+            // add noscript before images
+            $noscript = $DOM->createElement("noscript");
+            $noscript->appendChild($image->cloneNode());
+            $image->parentNode->insertBefore($noscript, $image);
+
+            // change src to data-normal
+            if ($existing_src) {
+                $image->removeAttribute("src");
+                $image->setAttribute("data-normal", $existing_src);
+            }
+
+            // change srcset to data-srcset
+            if ($existing_srcset) {
+                $image->removeAttribute("srcset");
+                $image->setAttribute("data-srcset", $existing_srcset);
+            }
+
+            // add _js class
+            $image->setAttribute("class", "_js {$image->getAttribute("class")}");
+        }
+
+        return $DOM->saveHTML();
+    }
+}
+add_filter("the_content", "new_site_lazy_load_images", 999);
+add_filter("acf_the_content", "new_site_lazy_load_images", 999);
+add_filter("post_thumbnail_html", "new_site_lazy_load_images", 999);
+
+// remove dimensions from thumbnails
+function new_site_remove_thumbnail_dimensions($html, $post_id, $post_image_id) {
+    if ($html) {
+        $DOM = new DOMDocument();
+        $DOM->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+        $images = $DOM->getElementsByTagName("img");
+
+        foreach ($images as $image) {
+            $image->removeAttribute("height");
+            $image->removeAttribute("width");
+        }
+
+        return $DOM->saveHTML();
+    }
+
+    return $html;
+}
+add_filter("post_thumbnail_html", "new_site_remove_thumbnail_dimensions", 10, 3);
 
 // add "Download Adobe Reader" link on all pages that link to PDFs
 function new_site_acrobat_link() {
@@ -357,69 +413,6 @@ function new_site_dequeue_nf_display() {
     wp_dequeue_style("nf-display");
 }
 add_action("ninja_forms_enqueue_scripts", "new_site_dequeue_nf_display", 999);
-/*removeIf(tribe_css_js_php)*/
-// force redirect 'cause tribe is stupid
-function new_site_tribe_redirect($template) {
-    if (is_post_type_archive("tribe_events")) {
-        return TEMPLATEPATH . "/archive-tribe_events.php";
-    } elseif (get_post_type() == "tribe_events") {
-        return TEMPLATEPATH . "/single-tribe_events.php";
-    } else {
-        return $template;
-    }
-}
-add_action("template_include", "new_site_tribe_redirect");
-
-// dequeue Tribe styles
-function new_site_tribe_events_dequeue_styles() {
-    wp_dequeue_style("tribe-events-calendar-style", 999);
-}
-add_filter("wp_enqueue_scripts", "new_site_tribe_events_dequeue_styles");
-
-// disable Tribe Events ical links (since I can't re-style them)
-function new_site_tribe_events_list_show_ical_link() {
-    return false;
-}
-add_filter("tribe_events_list_show_ical_link", "new_site_tribe_events_list_show_ical_link");
-
-// add class to event list date headers
-function new_site_tribe_events_list_the_date_headers($html, $event_month, $event_year) {
-    $event_month = DateTime::createFromFormat("Ymd", "{$event_year}{$event_month}01");
-    return "<h3 class='content_title title -divider'>{$event_month->format("F")} {$event_year}</h3>";
-}
-add_filter("tribe_events_list_the_date_headers", "new_site_tribe_events_list_the_date_headers", 10, 3);
-
-// add class to event list navigation links
-function new_site_tribe_the_nav_link($html) {
-    return preg_replace("/<a/", "<a class='menu-list_link link'", $html);
-}
-add_filter("tribe_events_the_previous_month_link", "new_site_tribe_the_nav_link", 10, 1);
-add_filter("tribe_events_the_next_month_link", "new_site_tribe_the_nav_link", 10, 1);
-add_filter("tribe_the_prev_event_link", "new_site_tribe_the_nav_link", 10, 1);
-add_filter("tribe_the_next_event_link", "new_site_tribe_the_nav_link", 10, 1);
-add_filter("tribe_the_day_link", "new_site_tribe_the_nav_link", 10, 1);/*endRemoveIf(tribe_css_js_php)*/
-
-// enable lazy loading on images
-function new_site_lazy_load_images($content) {
-    if (!((function_exists("tribe_is_month") && tribe_is_month()) && !is_tax())) {
-        // add `<noscript>` fallback for all imges
-        $content = preg_replace("/(<img[^>]+?>)/", "$1<noscript>$1</noscript>", $content);
-
-        // replace all `src` attributes in images with `data-nomral`
-        $content = preg_replace("/(<img.*?)(src=)([^>]+?><noscript)/im", "$1data-normal=$3", $content);
-
-        // replace all `srcset` attributes in images with `data-srcset`
-        $content = preg_replace("/(<img.*?)(srcset=)([^>]+?><noscript)/im", "$1data-srcset=$3", $content);
-
-        // add ` _js ` to all `class` attributes in images
-        $content = preg_replace("/(<img[^>]+class=(?:\'|\"))([^\'|\"\>]+)([^>]+?><noscript)/im", "$1_js $2$3", $content);
-    }
-
-    return $content;
-}
-add_filter("the_content", "new_site_lazy_load_images", 999, 1);
-add_filter("acf_the_content", "new_site_lazy_load_images", 999, 1);
-add_filter("post_thumbnail_html", "new_site_lazy_load_images", 999, 1);
 
 // redirect to the home template if no front page is set
 function new_site_home_template_redirect($template) {
@@ -430,3 +423,13 @@ function new_site_home_template_redirect($template) {
     }
 }
 add_action("template_include", "new_site_home_template_redirect");
+
+// decode HTML entities in bloginfo("description")
+function new_site_decode_html_entities_in_blog_description($value, $field) {
+    if ($field === "description") {
+        $value = html_entity_decode($value);
+    }
+
+    return $value;
+}
+add_filter("bloginfo", "new_site_decode_html_entities_in_blog_description", 10, 2);
--- src/functions/vendor.php
+++ src/functions/vendor.php
@@ -6,3 +6,6 @@
 // include page-for-post-type plugin
 require_once(TEMPLATEPATH . "/functions/vendor/page-for-post-type.php");
 add_action("after_setup_theme", array("Page_For_Post_Type", "get_instance"));
+
+// include wp14050 hotfix
+require_once(TEMPLATEPATH . "/functions/vendor/wp14050-hotfix.php");
