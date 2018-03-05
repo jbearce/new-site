e8fddaede00f754601de0cc78694e8fddcf2ea53
Jacob Bearce <jacob@bearce.me>
2018-03-05 13:24:45 -0600

    Add clean-up abilities to remove code task

--- gulp-tasks/init.js
+++ gulp-tasks/init.js
@@ -267,34 +267,80 @@ module.exports = {
 
         // remove modules selected during init
         const remove_selected_modules = (callback) => {
-            return gulp.src(global.settings.paths.src + "/**/*")
-                // check if a file is a binary
-                .pipe(plugins.is_binary())
-                // skip file if it's a binary
-                .pipe(plugins.through.obj((file, enc, next) => {
-                    if (file.isBinary()) {
-                        next();
-                        return;
-                    }
+            return new Promise ((resolve) => {
+                gulp.src(global.settings.paths.src + "/**/*")
+                    // check if a file is a binary
+                    .pipe(plugins.is_binary())
+                    // skip file if it's a binary
+                    .pipe(plugins.through.obj((file, enc, next) => {
+                        if (file.isBinary()) {
+                            next();
+                            return;
+                        }
 
-                    // go to next file
-                    next(null, file);
-                }))
+                        // go to next file
+                        next(null, file);
+                    }))
 
-                // remove resource code if selected
-                .pipe(plugins.remove_code({resources_html: project_data.remove_modules.indexOf("Resources") > -1 ? true : false, commentStart: "<!--", commentEnd: "-->"}))
-                .pipe(plugins.remove_code({resources_css_js_php: project_data.remove_modules.indexOf("Resources") > -1 ? true : false, commentStart: "/*", commentEnd: "*/"}))
+                    // remove resource code if selected
+                    .pipe(plugins.remove_code({resources_html: project_data.remove_modules.indexOf("Resources") > -1 ? true : false, commentStart: "<!--", commentEnd: "-->"}))
+                    .pipe(plugins.remove_code({resources_css_js_php: project_data.remove_modules.indexOf("Resources") > -1 ? true : false, commentStart: "/*", commentEnd: "*/"}))
 
-                // remove tribe code if selected
-                .pipe(plugins.remove_code({tribe_html: project_data.remove_modules.indexOf("Tribe Events") > -1 ? true : false, commentStart: "<!--", commentEnd: "-->"}))
-                .pipe(plugins.remove_code({tribe_css_js_php: project_data.remove_modules.indexOf("Tribe Events") > -1 ? true : false, commentStart: "/*", commentEnd: "*/"}))
+                    // remove tribe code if selected
+                    .pipe(plugins.remove_code({tribe_html: project_data.remove_modules.indexOf("Tribe Events") > -1 ? true : false, commentStart: "<!--", commentEnd: "-->"}))
+                    .pipe(plugins.remove_code({tribe_css_js_php: project_data.remove_modules.indexOf("Tribe Events") > -1 ? true : false, commentStart: "/*", commentEnd: "*/"}))
 
-                // output to source directory
-                .pipe(gulp.dest(global.settings.paths.src)).on("end", () => {
-                    // return the callback
-                    if (typeof callback === "function") {
-                        return callback();
-                    }
+                    // output to source directory
+                    .pipe(gulp.dest(global.settings.paths.src)).on("end", () => {
+                        // resolve the promise
+                        resolve();
+                    });
+                }).then(() => {
+                    return new Promise ((resolve) => {
+                        // remove any empty files
+                        plugins.glob(global.settings.paths.src + "/**/*", (err, files) => {
+                            files.forEach((file) => {
+                                if (plugins.fs.statSync(file).size <= 1) {
+                                    plugins.fs.unlinkSync(file);
+                                    console.log("\x1b[32m✔\x1b[0m deleted: " + plugins.path.relative(process.cwd(), file));
+                                }
+                            });
+
+                            // resolve the promise
+                            resolve();
+                        });
+                    }).then(() => {
+                        // remove any empty folders
+                        return new Promise ((resolve) => {
+                            plugins.delete_empty(global.settings.paths.src, () => {
+                                // resolve the promise
+                                resolve();
+                            });
+                        }).then(() => {
+                            // remove any remaining comments
+                            gulp.src(global.settings.paths.src + "/**/*")
+                                // check if a file is a binary
+                                .pipe(plugins.is_binary())
+                                // skip file if it's a binary
+                                .pipe(plugins.through.obj((file, enc, next) => {
+                                    if (file.isBinary()) {
+                                        next();
+                                        return;
+                                    }
+
+                                    // go to next file
+                                    next(null, file);
+                                }))
+                                .pipe(plugins.replace(/((?:\/\*|<!--)(?:end)?[rR]emoveIf\([^)]+\)(?:\*\/|-->))/g, ""))
+                                .pipe(gulp.dest(global.settings.paths.src))
+                                .on("end", () => {
+                                    // return the callback
+                                    if (typeof callback === "function") {
+                                        return callback();
+                                    }
+                                });
+                        });
+                    });
                 });
         };
 
