a34a4cd1ecdfbc7c65abdb9668e9856dfe503868
Jacob Bearce <jacob@bearce.me>
2018-03-05 13:36:16 -0600

    Fix some indentation

--- gulp-tasks/init.js
+++ gulp-tasks/init.js
@@ -295,53 +295,53 @@ module.exports = {
                         // resolve the promise
                         resolve();
                     });
-                }).then(() => {
-                    return new Promise ((resolve) => {
-                        // remove any empty files
-                        plugins.glob(global.settings.paths.src + "/**/*", (err, files) => {
-                            files.forEach((file) => {
-                                if (plugins.fs.statSync(file).size <= 1) {
-                                    plugins.fs.unlinkSync(file);
-                                    console.log("\x1b[32m✔\x1b[0m deleted: " + plugins.path.relative(process.cwd(), file));
-                                }
-                            });
+            }).then(() => {
+                return new Promise ((resolve) => {
+                    // remove any empty files
+                    plugins.glob(global.settings.paths.src + "/**/*", (err, files) => {
+                        files.forEach((file) => {
+                            if (plugins.fs.statSync(file).size <= 1) {
+                                plugins.fs.unlinkSync(file);
+                                console.log("\x1b[32m✔\x1b[0m deleted: " + plugins.path.relative(process.cwd(), file));
+                            }
+                        });
 
+                        // resolve the promise
+                        resolve();
+                    });
+                }).then(() => {
+                    // remove any empty folders
+                    return new Promise ((resolve) => {
+                        plugins.delete_empty(global.settings.paths.src, () => {
                             // resolve the promise
                             resolve();
                         });
                     }).then(() => {
-                        // remove any empty folders
-                        return new Promise ((resolve) => {
-                            plugins.delete_empty(global.settings.paths.src, () => {
-                                // resolve the promise
-                                resolve();
-                            });
-                        }).then(() => {
-                            // remove any remaining comments
-                            gulp.src(global.settings.paths.src + "/**/*")
-                                // check if a file is a binary
-                                .pipe(plugins.is_binary())
-                                // skip file if it's a binary
-                                .pipe(plugins.through.obj((file, enc, next) => {
-                                    if (file.isBinary()) {
-                                        next();
-                                        return;
-                                    }
+                        // remove any remaining comments
+                        gulp.src(global.settings.paths.src + "/**/*")
+                            // check if a file is a binary
+                            .pipe(plugins.is_binary())
+                            // skip file if it's a binary
+                            .pipe(plugins.through.obj((file, enc, next) => {
+                                if (file.isBinary()) {
+                                    next();
+                                    return;
+                                }
 
-                                    // go to next file
-                                    next(null, file);
-                                }))
-                                .pipe(plugins.replace(/((?:\/\*|<!--)(?:end)?[rR]emoveIf\([^)]+\)(?:\*\/|-->))/g, ""))
-                                .pipe(gulp.dest(global.settings.paths.src))
-                                .on("end", () => {
-                                    // return the callback
-                                    if (typeof callback === "function") {
-                                        return callback();
-                                    }
-                                });
-                        });
+                                // go to next file
+                                next(null, file);
+                            }))
+                            .pipe(plugins.replace(/((?:\/\*|<!--)(?:end)?[rR]emoveIf\([^)]+\)(?:\*\/|-->))/g, ""))
+                            .pipe(gulp.dest(global.settings.paths.src))
+                            .on("end", () => {
+                                // return the callback
+                                if (typeof callback === "function") {
+                                    return callback();
+                                }
+                            });
                     });
                 });
+            });
         };
 
         return new Promise ((resolve) => {
