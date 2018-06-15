f182c5311ed21ccc2e51ed9aafa9af173e93837c
Jacob Bearce <jacob@bearce.me>
2018-06-15 11:43:20 -0500

    Exclude margins from tooltip tribe-events-meta-list

--- src/tribe-events/month/tooltip.php
+++ src/tribe-events/month/tooltip.php
@@ -16,7 +16,7 @@
         </div>
 
 		<div class="tribe-events-event-body">
-            <ul class="tribe-events-meta-list">
+            <ul class="tribe-events-meta-list _nomargin">
                 <li class="tribe-events-meta-list-item">
                     <div class="tribe-event-duration">
                         <i class="tribe-events-meta-list-icon far fa-fw fa-clock"></i>
@@ -51,7 +51,7 @@
         </div>
 
 		<div class="tribe-events-event-body">
-            <ul class="tribe-events-meta-list">
+            <ul class="tribe-events-meta-list _nomargin">
                 <li class="tribe-event-meta-list-item">
                     <div class="tribe-event-duration">
                         <i class="tribe-events-meta-list-icon far fa-fw fa-clock"></i>
