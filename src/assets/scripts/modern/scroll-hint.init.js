a0cac178cee7d20f50dd36b204a8b86bbff1cfbe
Jacob Bearce <jacob@bearce.me>
2018-09-19 12:08:39 -0500

    Integrate scroll-hint; Close #117

---
+++ src/assets/scripts/modern/scroll-hint.init.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+// Scripts written by __gulp_init__author_name @ __gulp_init__author_company
+
+import ScrollHint from "scroll-hint";
+
+// init ScrollHint
+new ScrollHint(".text_table_container");
