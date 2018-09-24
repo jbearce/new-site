196e973b703b9f1c86efd7594fab5c0cd3c6bbdd
Jacob Bearce <jacob@bearce.me>
2018-09-24 18:10:52 -0500

    Disable the mobile menu on desktop

--- src/assets/scripts/modern/slideout.init.js
+++ src/assets/scripts/modern/slideout.init.js
@@ -5,30 +5,57 @@
 import Slideout from "slideout";
 
 // get the elements
-const PANEL = document.getElementById("page-container");
-const MENU  = document.getElementById("mobile-menu");
-const TOGGLE  = document.querySelector("[data-toggle=mobile-menu]");
+const PANEL        = document.getElementById("page-container");
+const MENU         = document.getElementById("mobile-menu");
+const TOGGLE       = document.querySelector("[data-toggle=mobile-menu]");
+const MOBILE_WIDTH = 768;
 
 // verify that the elements exist
 if (PANEL !== null && MENU !== null && TOGGLE !== null) {
     // initialize the menu
-    const MOBILE_MENU = new Slideout({
-        duration: 250,
-        menu:     MENU,
-        panel:    PANEL,
-    });
+    let mobile_menu = null;
 
     // toggle the menu when clicking on the toggle
     TOGGLE.addEventListener("click", (e) => {
         e.preventDefault();
-        MOBILE_MENU.toggle();
+
+        if (mobile_menu !== null) {
+            mobile_menu.toggle();
+        }
     });
 
     // close the menu when it's open and the content is clicked
     PANEL.addEventListener("click", (e) => {
-        if (e.target !== TOGGLE && MOBILE_MENU.isOpen()) {
+        if (mobile_menu !== null && e.target !== TOGGLE && mobile_menu.isOpen()) {
             e.preventDefault();
-            MOBILE_MENU.close();
+            mobile_menu.close();
+        }
+    });
+
+    const CONSTRUCT_MENU = () => {
+        return new Slideout({
+            duration: 250,
+            menu: MENU,
+            panel: PANEL,
+        });
+    };
+
+    // destroy the menu on desktop
+    window.addEventListener("load", () => {
+        if (mobile_menu === null && document.documentElement.clientWidth < MOBILE_WIDTH) {
+            mobile_menu = CONSTRUCT_MENU();
+        }
+    });
+
+    // destroy the menu on desktop
+    window.addEventListener("resize", () => {
+        const CLIENT_WIDTH = document.documentElement.clientWidth;
+
+        if (mobile_menu !== null && CLIENT_WIDTH >= MOBILE_WIDTH) {
+            mobile_menu.destroy();
+            mobile_menu = null;
+        } else if (mobile_menu === null && CLIENT_WIDTH < MOBILE_WIDTH) {
+            mobile_menu = CONSTRUCT_MENU();
         }
     });
 }
