a6b0b2bdc28e6aab0354cca2f654407093ecf41c
Jacob Bearce <jacob@bearce.me>
2018-09-29 10:34:49 -0500

    Clean up formatting of scripts task; Merge linting step

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -5,46 +5,42 @@
 module.exports = {
     scripts(gulp, plugins, ran_tasks, on_error) {
         // task-specific plugins
-        const eslint  = require("gulp-eslint");
-        const webpack = require("webpack-stream");
-
-        // lint custom scripts
-        const lint_scripts = (js_directory, compare_file_name = "modern.js", source = [global.settings.paths.src + "/assets/scripts/**/*.js"]) => {
-            return gulp.src(source)
-                // prevent breaking on error
-                .pipe(plugins.plumber({errorHandler: on_error}))
-                // check if source is newer than destination
-                .pipe(plugins.newer(js_directory + "/" + compare_file_name))
-                // lint all non-vendor scripts
-                .pipe(eslint())
-                // print lint errors
-                .pipe(eslint.format());
-        };
+        const ESLINT  = require("gulp-eslint");
+        const WEBPACK = require("webpack-stream");
 
         // process scripts
-        const process_scripts = (js_directory, destination_file_name = "modern.js", compare_file_name = "modern.js", source = [global.settings.paths.src + "/assets/scripts/*.js"]) => {
+        const PROCESS_SCRIPTS = (js_directory, destination_file_name = "modern.js", compare_file_name = "modern.js", source = [global.settings.paths.src + "/assets/scripts/*.js"]) => {
             return new Promise((resolve, reject) => {
-                const webpack_config = {
+                const WEBPACK_CONFIG = {
                     mode: "development",
                 };
 
                 // update webpack config for the current target destination and file name
-                webpack_config.mode   = plugins.argv.dist ? "production" : webpack_config.mode;
-                webpack_config.output = {filename: destination_file_name};
+                WEBPACK_CONFIG.mode   = plugins.argv.dist ? "production" : WEBPACK_CONFIG.mode;
+                WEBPACK_CONFIG.output = {
+                    filename: destination_file_name
+                };
 
                 const TASK = gulp.src(source)
                     // prevent breaking on error
                     .pipe(plugins.plumber({errorHandler: on_error}))
                     // check if source is newer than destination
                     .pipe(plugins.newer(js_directory + "/" + compare_file_name))
+                    // lint all scripts
+                    .pipe(ESLINT())
+                    // print lint errors
+                    .pipe(ESLINT.format())
                     // run webpack
-                    .pipe(webpack(webpack_config))
+                    .pipe(WEBPACK(WEBPACK_CONFIG))
                     // generate a hash and add it to the file name
                     .pipe(plugins.hash({template: "<%= name %>.<%= hash %><%= ext %>"}))
                     // output scripts to compiled directory
                     .pipe(gulp.dest(js_directory))
                     // generate a hash manfiest
-                    .pipe(plugins.hash.manifest(".hashmanifest-scripts", {deleteOld: true, sourceDir: js_directory}))
+                    .pipe(plugins.hash.manifest(".hashmanifest-scripts", {
+                        deleteOld: true,
+                        sourceDir: js_directory
+                    }))
                     // output hash manifest in root
                     .pipe(gulp.dest("."))
                     // reject after errors
@@ -61,47 +57,47 @@ module.exports = {
         // scripts task, lints, concatenates, & compresses JS
         return new Promise ((resolve) => {
             // set JS directory
-            const js_directory = plugins.argv.dist ? global.settings.paths.dist + "/assets/scripts" : global.settings.paths.dev + "/assets/scripts";
+            const JS_DIRECTORY = plugins.argv.dist ? global.settings.paths.dist + "/assets/scripts" : global.settings.paths.dev + "/assets/scripts";
 
             // set the source directory
-            const source_directory = global.settings.paths.src + "/assets/scripts";
+            const SOURCE_DIRECTORY = global.settings.paths.src + "/assets/scripts";
 
             // set up an empty merged stream
-            const merged_streams = plugins.merge();
+            const MERGED_STREAMS = plugins.merge();
             // get the script source folder list
-            const script_folders = plugins.fs.readdirSync(source_directory);
+            const SCRIPT_FOLDERS = plugins.fs.readdirSync(SOURCE_DIRECTORY);
             // get the script destination file list
-            const script_files   = plugins.fs.existsSync(js_directory) ? plugins.fs.readdirSync(js_directory) : false;
+            const SCRIPT_FILES   = plugins.fs.existsSync(JS_DIRECTORY) ? plugins.fs.readdirSync(JS_DIRECTORY) : false;
 
             // process all the script folders
-            const process_script_folders = () => {
+            const PROCESS_SCRIPT_FOLDERS = () => {
                 return new Promise((resolve) => {
-                    const folder_name = script_folders.shift();
-                    const file_name   = script_files ? script_files.find((name) => {
-                        return name.match(new RegExp(folder_name + ".[a-z0-9]{8}.js"));
-                    }) : folder_name + ".js";
-
-                    // lint all scripts, except for critical, update the stream
-                    if (folder_name !== "critical") {
-                        const linted = lint_scripts(js_directory, file_name, source_directory + "/" + folder_name + "/**/*");
-                        merged_streams.add(linted);
-                    }
+                    const FOLDER_NAME = SCRIPT_FOLDERS.shift();
+                    const FILE_NAME   = SCRIPT_FILES ? SCRIPT_FILES.find((name) => {
+                        return name.match(new RegExp(FOLDER_NAME + ".[a-z0-9]{8}.js"));
+                    }) : FOLDER_NAME + ".js";
 
                     // process all scripts, update the stream
-                    process_scripts(js_directory, folder_name + ".js", file_name, source_directory + "/" + folder_name + "/**/*").then((processed) => {
-                        merged_streams.add(processed);
+                    PROCESS_SCRIPTS(JS_DIRECTORY, FOLDER_NAME + ".js", FILE_NAME, SOURCE_DIRECTORY + "/" + FOLDER_NAME + "/**/*").then((processed) => {
+                        MERGED_STREAMS.add(processed);
                         resolve();
                     });
-                }).then(() => script_folders.length > 0 ? process_script_folders() : resolve()); // loop again if foldres remain, otherwise resolve
+                }).then(() => SCRIPT_FOLDERS.length > 0 ? PROCESS_SCRIPT_FOLDERS() : resolve()); // loop again if foldres remain, otherwise resolve
             };
 
-            return process_script_folders().then(() => {
-                // merge all five steams back in to one
-                return merged_streams
+            return PROCESS_SCRIPT_FOLDERS().then(() => {
+                // wrap up
+                return MERGED_STREAMS
                     // prevent breaking on error
-                    .pipe(plugins.plumber({errorHandler: on_error}))
+                    .pipe(plugins.plumber({
+                        errorHandler: on_error,
+                    }))
                     // notify that task is complete, if not part of default or watch
-                    .pipe(plugins.gulpif(gulp.seq.indexOf("scripts") > gulp.seq.indexOf("default"), plugins.notify({title: "Success!", message: "Scripts task complete!", onLast: true})))
+                    .pipe(plugins.gulpif(gulp.seq.indexOf("scripts") > gulp.seq.indexOf("default"), plugins.notify({
+                        title:   "Success!",
+                        message: "Scripts task complete!",
+                        onLast:  true,
+                    })))
                     // push task to ran_tasks array
                     .on("data", () => {
                         if (ran_tasks.indexOf("scripts") < 0) {
