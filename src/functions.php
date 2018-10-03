31253e65367cd74f3b9438953243746c2ba74ab8
Jacob Bearce <jacob@bearce.me>
2018-10-02 19:57:56 -0500

    Move custom functions to the top of functions.php; Close #163

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,6 @@
 <?php
 require_once(get_theme_file_path("/functions/vendor.php"));
+require_once(get_theme_file_path("/functions/custom-functions.php"));
 require_once(get_theme_file_path("/functions/post-types.php"));
 require_once(get_theme_file_path("/functions/page-speed.php"));
 require_once(get_theme_file_path("/functions/theme-features.php"));
@@ -9,6 +10,5 @@ require_once(get_theme_file_path("/functions/styles-scripts.php"));
 require_once(get_theme_file_path("/functions/image-sizes.php"));
 require_once(get_theme_file_path("/functions/filters.php"));
 require_once(get_theme_file_path("/functions/shortcodes.php"));
-require_once(get_theme_file_path("/functions/custom-functions.php"));
 require_once(get_theme_file_path("/functions/advanced-custom-fields.php"));
 require_once(get_theme_file_path("/functions/tribe-events.php"));
