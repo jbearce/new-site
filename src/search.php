ffab61d1da65aa7ba56f8b74f54272c0062addee
Jacob Bearce <JacobDB@users.noreply.github.com>
2018-10-03 10:14:42 -0500

    Update "no search query" phrasing

--- src/search.php
+++ src/search.php
@@ -14,7 +14,7 @@
                     __gulp_init_namespace___get_template_part("partials/articles/post-excerpt.php", array("post" => $post, "class" => "content__article"));
                 }
             } else {
-                __gulp_init_namespace___get_template_part("partials/articles/post-none.php", array("class" => "content__article", "error" => !get_search_query() ? __("No search query was entered.", "__gulp_init_namespace__") : __gulp_init_namespace___get_no_posts_message(get_queried_object())));
+                __gulp_init_namespace___get_template_part("partials/articles/post-none.php", array("class" => "content__article", "error" => !get_search_query() ? __("No search phrase was entered.", "__gulp_init_namespace__") : __gulp_init_namespace___get_no_posts_message(get_queried_object())));
             }
             ?>
 
