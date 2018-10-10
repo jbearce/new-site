e474bd65354fbf4c6ff74ecbb30abe9708caed88
Jacob Bearce <jacob@bearce.me>
2018-10-10 11:01:58 -0500

    Automatically buildu webpack entry points; Close #168

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,20 @@ const GLOB = require("glob");
 
 module.exports = {
     config(plugins, source_directory, js_directory) {
+        const ENTRY = {};
+
+        const SCRIPT_FOLDERS = plugins.fs.readdirSync(source_directory);
+
+        // automatically build entry points based on folders in src/assets/scripts
+        if (SCRIPT_FOLDERS.length > 0) {
+            SCRIPT_FOLDERS.forEach((folder) => {
+                ENTRY[folder] = GLOB.sync(source_directory + "/" + folder + "/**/*.js");
+            });
+        }
+
         return {
             mode:   plugins.argv.dist ? "production" : "development",
-            entry:  {
-                "critical":       GLOB.sync(source_directory + "/critical/**/*.js"),
-                "legacy":         GLOB.sync(source_directory + "/legacy/**/*.js"),
-                "modern":         GLOB.sync(source_directory + "/modern/**/*.js"),
-                "service-worker": GLOB.sync(source_directory + "/service-worker/**/*.js"),
-            },
+            entry:  ENTRY,
             output: {
                 path:     plugins.path.resolve(__dirname, js_directory),
                 filename: "[name].js",
