fa662f9a7ba111111e987573d74dba7e4d6ca70f
Jacob Bearce <jacob@bearce.me>
2018-11-01 10:58:31 -0500

    Add a cookie to prevent nagging the user about being offline; Close #148

--- src/assets/scripts/modern/connectivity.js
+++ src/assets/scripts/modern/connectivity.js
@@ -7,6 +7,12 @@ import Noty from "noty";
 let is_offline = false;
 
 const OFFLINE_NOTY = new Noty({
+    callbacks: {
+        afterClose: () => {
+            // store a cookie so the user isn't nagged on every page load
+            document.cookie = "__gulp_init_namespace___offline_noty_dismissed=true; max-age=" + (60 * 60 * 6);
+        },
+    },
     text: l10n.noty.offline.text,
     theme: false,
 });
@@ -18,9 +24,15 @@ const UPDATE_STATUS = () => {
         document.documentElement.classList.toggle("is-offline", is_offline);
 
         if (is_offline) {
-            OFFLINE_NOTY.show();
+            // check a cookie so the user isn't nagged on every page load
+            if (!document.cookie.match(/__gulp_init_namespace___offline_noty_dismissed=true/)) {
+                OFFLINE_NOTY.show();
+            }
         } else {
             OFFLINE_NOTY.close();
+
+            // set the cookie to false so that prompt will appear again next time they go offline
+            document.cookie = "__gulp_init_namespace___offline_noty_dismissed=false; max-age=" + (60 * 60 * 6);
         }
     }
 };
