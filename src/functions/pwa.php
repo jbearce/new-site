089913c456ca9186eac693e6ddd7aa54c0196b26
Jacob Bearce <jacob@bearce.me>
2018-11-01 10:34:48 -0500

    Fix path to 512x512 icon

--- src/functions/pwa.php
+++ src/functions/pwa.php
@@ -28,7 +28,7 @@ function __gulp_init_namespace___construct_manifest($template) {
             "theme_color"      => $theme_color ? $theme_color : "<%= pwa_theme_color %>",
             "icons"            => array(
                 array(
-                    "src"   => get_theme_file_uri("assets/media/android/launcher-icon-512x512"),
+                    "src"   => get_theme_file_uri("assets/media/android/launcher-icon-512x512.png"),
                     "type"  => "image/png",
                     "sizes" => "512x512",
                 ),
