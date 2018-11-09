6e9fa6ae34da0cca52a5a0ce17ac181c48b2cab4
Jacob Bearce <jacob@bearce.me>
2018-11-09 13:49:47 -0600

    Simplify HTML source; Close #182

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -42,7 +42,7 @@ module.exports = {
         };
 
         // process HTML
-        const PROCESS_HTML = (html_directory, source = [`${global.settings.paths.src}/**/*`, `!${global.settings.paths.src}{/assets,/assets/**}`]) => {
+        const PROCESS_HTML = (html_directory, source = [`${global.settings.paths.src}/**/*`, `!${global.settings.paths.src}/assets`]) => {
             // read data from package.json
             const JSON_DATA = plugins.json.readFileSync("package.json");
 
