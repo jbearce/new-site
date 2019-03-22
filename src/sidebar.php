caa3aa9fc46d41d0edd9990317eccbc3da9869aa
Jacob Bearce <jacob@bearce.me>
2019-03-22 11:26:03 -0500

    Clean up 'sub_menu' sidebar markup

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,18 +1,16 @@
 <?php
-$sub_menu = wp_nav_menu(array(
-    "container"            => false,
-    "direct_parent"        => true,
-    "depth"                => 3,
-    "echo"                 => false,
-    "items_wrap"           => "<ul class='menu-list menu-list--submenu menu-list--vertical'>%3\$s</ul>",
-    "show_parent"          => false,
-    "sub_menu"             => true,
-    "theme_location"       => "primary",
-    "tree_mode"            => "viewed",
-    "walker"               => new __gulp_init_namespace___menu_walker(),
-));
-
-$sub_menu = preg_match_all("/<a/im", $sub_menu, $matches) > 0 ? $sub_menu : false;
+$sub_menu = has_nav_menu("primary") ? wp_nav_menu(array(
+    "container"      => false,
+    "direct_parent"  => true,
+    "depth"          => 3,
+    "echo"           => false,
+    "items_wrap"     => "<ul class='menu-list menu-list--submenu menu-list--vertical'>%3\$s</ul>",
+    "show_parent"    => false,
+    "sub_menu"       => true,
+    "theme_location" => "primary",
+    "tree_mode"      => "viewed",
+    "walker"         => new __gulp_init_namespace___menu_walker(),
+)) : false;
 ?>
 
 <?php if ($sub_menu): ?>
