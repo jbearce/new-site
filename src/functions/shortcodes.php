bb7242551fe86b642b4291179eee5c039151f395
Jacob Bearce <jacob@bearce.me>
2019-04-01 14:00:23 -0500

    Improve comments in shortcodes

--- src/functions/shortcodes.php
+++ src/functions/shortcodes.php
@@ -3,7 +3,10 @@
  * Functions: Shortcodes
 \* ------------------------------------------------------------------------ */
 
-// add row shortcode
+/**
+ * Shortcode for users to add rows to the content region
+ * @example [row class="row--padded row--tight"]
+ */
 function __gulp_init_namespace___row_shortcode($atts, $content = null) {
     extract(shortcode_atts(
         array(
@@ -18,7 +21,10 @@ function __gulp_init_namespace___row_shortcode($atts, $content = null) {
 }
 add_shortcode("row", "__gulp_init_namespace___row_shortcode");
 
-// add col shortcode
+/**
+ * Shortcode for usrs to add columns to the content region
+ * @example [col mobile=12 tablet=6 notebook=4 desktop=auto class="col--nogrow col--noshrink"]
+ */
 function __gulp_init_namespace___col_shortcode($atts , $content = null) {
     extract(shortcode_atts(
         array(
@@ -30,12 +36,12 @@ function __gulp_init_namespace___col_shortcode($atts , $content = null) {
         ), $atts
     ));
 
-    $value =  $mobile   ? "-{$mobile}"         : "-auto";
+    $value =  $mobile   ? "col-{$mobile}"         : "col-auto";
     $value .= $tablet   ? " col-xs-{$tablet}"  : "";
     $value .= $notebook ? " col-l-{$notebook}" : "";
     $value .= $desktop  ? " col-xl-{$desktop}" : "";
     $value .= $class    ? " $class"            : "";
 
-    return "<div class='col{$value}'>" . do_shortcode($content) . "</div>";
+    return "<div class='{$value}'>" . do_shortcode($content) . "</div>";
 }
 add_shortcode("col", "__gulp_init_namespace___col_shortcode");
