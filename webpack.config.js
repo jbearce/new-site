b3e1b21ba35d44abd03da92e5d4c04621d4cac00
Jacob Bearce <jacob@bearce.me>
2019-08-19 08:53:21 -0500

    Add check for `.mjs` files for babel transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             module:  {
                 rules: [
                     {
-                        test: /\.js$/,
+                        test: /\.m?js$/,
                         use: {
                             loader: "babel-loader",
                             options: {
